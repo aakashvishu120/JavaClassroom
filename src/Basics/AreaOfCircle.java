402eae85c55715d0833357ec24caa37c80dece36
Aakash Vishwakarma <aakashvishu120@gmail.com>
2025-07-17 15:30:22 +0530

    [Aakash] Update : Update input Message

--- src/Basics/AreaOfCircle.java
+++ src/Basics/AreaOfCircle.java
@@ -7,7 +7,7 @@ public class AreaOfCircle {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the Temp in Celsius");
+		System.out.println("Enter The Radius");
 		double radius = sc.nextFloat();
 		double area = Math.PI * radius * radius;
 		System.out.println("Area of the Circle : " + area);
