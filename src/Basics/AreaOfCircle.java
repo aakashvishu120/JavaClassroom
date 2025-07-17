94107a8b6be54b78f2598c0170d3096fe57406a3
Aakash Vishwakarma <aakashvishu120@gmail.com>
2025-07-17 15:26:43 +0530

    [Aakash] Create : Area of a Circle

---
+++ src/Basics/AreaOfCircle.java
@@ -0,0 +1,17 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class AreaOfCircle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Temp in Celsius");
+		double radius = sc.nextFloat();
+		double area = Math.PI * radius * radius;
+		System.out.println("Area of the Circle : " + area);
+		sc.close();
+	}
+
+}
