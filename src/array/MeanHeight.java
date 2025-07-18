5f23439488343634de254805f94d5faa6e4970eb
Aakash Vishwakarma <aakashvishu120@gmail.com>
2025-07-18 15:09:32 +0530

    [Aakash] Add : Mean Height of Players

---
+++ src/array/MeanHeight.java
@@ -0,0 +1,28 @@
+package array;
+
+import java.util.Scanner;
+
+public class MeanHeight {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+
+		double[] heights = new double[11];
+		double sum = 0, mean = 0;
+
+		System.out.println("Enter the height of 11 Players");
+		for (int i = 0; i < 11; i++) {
+			heights[i] = scanner.nextDouble();
+		}
+
+		for (int i = 0; i < 11; i++) {
+			sum += heights[i];
+		}
+
+		mean = sum / 11;
+		System.out.print("Mean Height of the Players : ");
+		System.out.println(mean);
+	}
+
+}
