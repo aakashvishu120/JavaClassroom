3b3cbb201f10b26a2848bab5d4718712e320809c
Aakash Vishwakarma <aakashvishu120@gmail.com>
2025-07-18 14:38:46 +0530

    [Aakash] Add : Add Multiplication of the given number

---
+++ src/array/Multiplication.java
@@ -0,0 +1,23 @@
+package array;
+
+import java.util.Scanner;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Enter the number : ");
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+		int[] mutiplication = new int[11];
+		for(int i =1 ; i <= 10 ; i++) {
+			mutiplication[i] = number * i;
+		}
+		
+		System.out.println("Multiplication of the Given Number");
+		for(int i = 1 ; i <= 10 ; i++) {
+			System.out.println(number + " * " + i + " = " + mutiplication[i]);
+		}
+	}
+
+}
