a390db5923350a95cb0a86abab3a79f351cc85a1
Aakash Vishwakarma <aakashvishu120@gmail.com>
2025-07-21 11:26:17 +0530

    [Aakash] Read : Check the performance of StringBuilder Class

---
+++ src/mystrings/MyStringBuilder.java
@@ -0,0 +1,15 @@
+package mystrings;
+
+public class MyStringBuilder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuilder sb = new StringBuilder();
+		for(int i =0 ; i < 26 ; i++) {
+			char ch = (char)('a' + i);
+			sb.append(ch);
+		}
+		System.out.println(sb.toString());
+	}
+
+}
